baa167f0ce37502162c31c840cceaefa4b501a27
Efrain Francisco Julca Vivanco <shinigami_56@hotmail.com>
2019-02-16 00:26:47 -0500

    repository patron

--- Shop/Shop.Web/Startup.cs
+++ Shop/Shop.Web/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shop.Web.Data;
+using Shop.Web.Data.Entities;
 
 namespace Shop.Web
 {
@@ -30,6 +31,10 @@ namespace Shop.Web
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddTransient<SeedDB>();
+
+            services.AddScoped<IRepository, Repository>()
 
             services.Configure<CookiePolicyOptions>(options =>
             {
