ab473aca81d2e3a7fc43f40275e10b3756e67914
Efrain Francisco Julca Vivanco <shinigami_56@hotmail.com>
2019-02-27 09:40:40 -0500

    3

---
+++ Shop/Shop.Web/Data/SeedDB.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data.Entities
+{
+    public class SeedDB
+    {
+        private readonly DataContext context;
+        private readonly UserManager<User> userManager;
+        private Random random;
+
+        public SeedDB(DataContext context,UserManager<User> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+            this.random = new Random();
+        }
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            var user = await this.userManager.FindByEmailAsync("efraju.56@gmail.com");
+            if (user==null)
+            {
+                user = new User
+                {
+                    FirstName = "Efra",
+                    LastName = "Julca",
+                    Email = "efraju.56@gmail.com",
+                    UserName = "efraju.56@gmail.com",
+                    PhoneNumber="984231351"
+                };
+
+                var result = await this.userManager.CreateAsync(user, "123456");
+                if (result!=IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
+            if (!this.context.Products.Any())
+            {
+                this.AddProduct("Agua Cielo",user);
+                this.AddProduct("Celular", user);
+                this.AddProduct("Laptop Lenovo", user);
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name,User user)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(1000),
+                IsAvailabe = true,
+                Stock = this.random.Next(100),
+                User=user
+            });
+        }
+    }
+}
---
+++ Shop/Shop.Web/Helpers/UserHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Shop.Web.Data.Entities;
+
+namespace Shop.Web.Helpers
+{
+    public class UserHelper : IUserHelper
+    {
+        private readonly UserManager<User> userMAnager;
+
+        public UserHelper(UserManager<User> userMAnager)
+        {
+            this.userMAnager = userMAnager;
+        }
+
+        public async Task<IdentityResult> AddUserAsync(User user, string password)
+        {
+            return await this.userMAnager.CreateAsync(user, password);
+        }
+
+        public Task<User> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
